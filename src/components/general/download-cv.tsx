d6a1dd7075a07a361167a4884dc47bdb17d38dab
Youssef Fahmy <youssefhosny75@gmail.com>
2024-12-14 22:39:35 +0200

    Update download-cv.tsx

--- src/components/general/download-cv.tsx
+++ src/components/general/download-cv.tsx
@@ -5,8 +5,8 @@ import Button from '@/components/general/button';
 const DownloadCV = () => {
   const handleDownload = () => {
     const link = document.createElement('a');
-    link.href = 'https://drive.google.com/file/d/1YLFVLbIvL7-heRL6PG5pcfP3qTkgBgwY/view?usp=sharing';
-    link.download = 'Youssef_Fahmy_CV.pdf'; // Name of the file when downloaded
+    link.href = 'https://drive.google.com/file/d/1edWG94wCRFbcw7Dk7wZpbUyn2O5Nq4Zx/view?usp=sharing';
+    link.download = 'Youssef_Hosny_CV.pdf'; // Name of the file when downloaded
     document.body.appendChild(link);
     link.click();
     document.body.removeChild(link);
