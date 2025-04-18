6bf7144aa5e8cfb129d3398fe411226856947b3c
Youssef Fahmy <youssefhosny75@gmail.com>
2025-04-19 01:09:21 +0200

    Update work.tsx

--- src/components/sections/work.tsx
+++ src/components/sections/work.tsx
@@ -34,6 +34,7 @@ const WorkSection = () => {
           <option value="Looker">Looker</option>
           <option value="Tableau">Tableau</option>
           <option value="Python">Python</option>
+          <option value="SQL">Excel</option>
           <option value="Excel">Excel</option>
           {/* Add more options as needed */}
         </select>
